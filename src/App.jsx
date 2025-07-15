0f5f323d1754382c4f933750555279a334b636b2
Robin Ryan <robin1ryan@hotmail.com>
2025-07-15 13:13:38 +0300

    same as previous

--- src/App.jsx
+++ src/App.jsx
@@ -153,9 +153,6 @@ export default App;
  *“A typeface that is odd and oval, why not call it “Oddval,” suggested the designers at FourPlus. We immediately fell in love with it. We knew the name might sound strange but wasn’t that the point from the beginning?
  *
 
-
-
-
  *
  * Make so the navbar changes color and match whatever section screen currently is over
  * also apply it for +/x button when window is smaller
@@ -171,4 +168,7 @@ export default App;
  * on-page reload, reset view-point to top
  * currently, navbar doesnt change color if you change page
  * navbutton on small screens (x/+) needs to change color with navbar
+ * 
+ * add cookies button when page is first loaded
+ * maybe make it so that cookies are saved when you've clicked accept or not accept
  */
